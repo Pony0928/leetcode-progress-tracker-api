2b7749800de322086d0e5feece3a660bd17f555a
Jingjing Ma <jingjing.ma928@gmail.com>
2026-08-04 18:52:24 -0700

    Add Problem Pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from typing import Literal
 
 app = FastAPI(title="LeetCode Progress Tracker API")
+
+
+class Problem(BaseModel):
+    number: int
+    title: str
+    difficulty: Literal["Easy", "Medium", "Hard"]
+    topic: str
+    url: HttpUrl
 
 
 @app.get("/health")
