7378dfbf8aaf3596f8bf3359b70b8731368f9d16
Jingjing Ma <jingjing.ma928@gmail.com>
2026-08-27 18:28:19 -0700

    feat: add UserDB model with unique email/username constraints

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 class ProblemDB(Base):
@@ -10,3 +11,12 @@ class ProblemDB(Base):
     difficulty = Column(String)
     topic = Column(String)
     url = Column(String)
+
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
